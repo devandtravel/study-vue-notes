4b575b7d11c692961896e6fab8f1b7064618b3a5
devandtravel <devandtravel@gmail.com>
2021-08-14 14:03:34 +0300

    add: notes list, input for new note, delete button

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const App = {
     return {
       placeholderString: 'Note title',
       title: 'Notes list',
-      inputValue: ''
+      inputValue: '',
+      notes: ['note 1', 'note 2', 'note 3', 'note 4', 'note 5']
     }
   },
-  methods: {}
+  methods: {
+    addNote() {
+      trimmedInputValue = this.inputValue.trim()
+      if (trimmedInputValue) {
+        this.notes.push(trimmedInputValue)
+        this.inputValue = ''
+      }
+    },
+    deleteNote(index) {
+      this.notes.splice(index, 1)
+    }
+  }
 }
 
 Vue.createApp(App).mount('#app')
